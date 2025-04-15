f77e26bba5ac21c123ba5115614ff2df25416bfe
Taehee <taeheecafe@naver.com>
2025-04-15 13:30:08 +0900

    UserRepository 생성

--- backend/src/main/java/com/example/oauthjwt/repository/UserRepository.java
+++ backend/src/main/java/com/example/oauthjwt/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.oauthjwt.repository;
 
+import com.example.oauthjwt.entity.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.oauthjwt.entity.User;
 import java.util.Optional;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
@@ -13,6 +15,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
+    List<User> findByStatus(UserStatus status);
 
+    boolean existsByEmail(String email);
 
+    Optional<User> findByEmailAnsStatus(String email, UserStatus status);
+
+    Optional<User> findByEmailAndUsername(String email,String username);
 }
