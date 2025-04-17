76c3df6ea69a526d136ac50a1e69486a51f181b5
이승찬 <79355080+LeeSeungChan-1@users.noreply.github.com>
2025-04-17 09:52:17 +0900

    transaction 기능 시작

--- backend/src/main/java/com/example/oauthjwt/entity/TRANSACTION_STATE.java
+++ backend/src/main/java/com/example/oauthjwt/entity/TRANSACTION_STATE.java
@@ -1,8 +1,8 @@
 package com.example.oauthjwt.entity;
 
 public enum TRANSACTION_STATE {
-    PENDING,
-    COMPLETED,
-    FAILED,
-    REFUNDED
+    PENDING, // 시작 전
+    COMPLETED, // 완료
+    FAILED, // 실패
+    REFUNDED // 환불
 }
