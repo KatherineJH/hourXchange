b442fc9d170b8ca5e71d94c7b98609e1c2744046
이승찬 <79355080+LeeSeungChan-1@users.noreply.github.com>
2025-04-23 13:48:10 +0900

    root router serviceProduct 연결

--- client-app/src/router/root.jsx
+++ client-app/src/router/root.jsx
@@ -3,6 +3,7 @@ import Main from "../page/Main.jsx";
 import Homepage from "../component/homepage/Homepage.jsx";
 import ChatRoom from "../component/chat/ChatRoom.jsx"; // WebSocket 채팅방
 import UserRouter from "./UserRouter.jsx";
+import serviceProduct from "./serviceProduct.jsx";
 
 const root = createBrowserRouter([
   {
@@ -14,6 +15,11 @@ const root = createBrowserRouter([
       ...UserRouter(), // /chat 등
     ],
   },
+  {
+    path: "serviceProduct",
+    element: <Main />,
+    children: serviceProduct(),
+  }
 ]);
 
 export default root;
