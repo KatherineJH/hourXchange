85d9baef4e5a2a4528baeb3485c9e5e5faf04ae5
KatherineJH <ohhomoya@gmail.com>
2025-04-23 10:37:55 +0900

    first commit
    first commit

--- client-app/src/router/root.jsx
+++ client-app/src/router/root.jsx
@@ -1,21 +1,19 @@
-import {createBrowserRouter} from "react-router-dom"
-import {lazy} from "react"
-
-const Main = lazy(() => import("../page/Main.jsx"))
-import employeeRouter from "./employeeRouter.jsx";
-
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../page/Main.jsx";
+import Homepage from "../component/homepage/Homepage.jsx";
+import ChatRoom from "../component/chat/ChatRoom.jsx"; // WebSocket 채팅방
+import UserRouter from "./UserRouter.jsx";
 
 const root = createBrowserRouter([
-    {
-        path: '',
-        element: <Main/>,
-        children: [{index: true}]
-    },
-    {
-        path: 'chat',
-        element: <Main/>,
-        children: employeeRouter()
-    }
-])
+  {
+    path: "",
+    element: <Main />, 
+    children: [
+      { index: true, element: <Homepage /> },
+      { path: "chat-room/:chatRoomId", element: <ChatRoom /> },
+      ...UserRouter(), // /chat 등
+    ],
+  },
+]);
 
-export default root
+export default root;
