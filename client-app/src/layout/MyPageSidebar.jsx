5723d32a7fc093b9fc4b05d590bfb9ad9a94ea46
Taehee <taeheecafe@naver.com>
2025-05-12 18:58:15 +0900

    temp

--- client-app/src/layout/MyPageSidebar.jsx
+++ client-app/src/layout/MyPageSidebar.jsx
@@ -2,41 +2,42 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
-    Box,
-    List,
-    ListItemButton,
-    ListItemText,
-    Divider,
-    RadioGroup,
-    Radio,
-    FormControlLabel,
-    Typography,
-    Collapse,
+  Box,
+  List,
+  ListItemButton,
+  ListItemText,
+  Divider,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
+  Typography,
+  Collapse,
 } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
+import MyList from "../component/transaction/MyList";
 
 const menu = [
-    { text: "메뉴1", to: "/product/buy" },
-    { text: "메뉴2", to: "/product/sell" },
-    { text: "메뉴3", to: "/product/volunteer" },
+  { text: "메뉴1", to: "/product/buy" },
+  { text: "메뉴2", to: "/myPage/my" },
+  { text: "메뉴3", to: "/product/volunteer" },
 ];
 
 const Sidebar = () => {
-    return (
-        <Box component="nav" sx={{ p: 2 }}>
-            <List>
-                {menu.map((item) => (
-                    <ListItemButton
-                        key={item.text}
-                        component={RouterLink} // React Router <Link> 로 동작
-                        to={item.to} // 이동할 경로
-                    >
-                        <ListItemText primary={item.text} />
-                    </ListItemButton>
-                ))}
-            </List>
-        </Box>
-    );
+  return (
+    <Box component="nav" sx={{ p: 2 }}>
+      <List>
+        {menu.map((item) => (
+          <ListItemButton
+            key={item.text}
+            component={RouterLink} // React Router <Link> 로 동작
+            to={item.to} // 이동할 경로
+          >
+            <ListItemText primary={item.text} />
+          </ListItemButton>
+        ))}
+      </List>
+    </Box>
+  );
 };
 
 export default Sidebar;
