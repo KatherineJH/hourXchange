50213a4423d42271f35b646da4087da27c061d2d
KatherineJH <ohhomoya@gmail.com>
2025-05-26 19:43:17 +0900

    docker compose up 확인 완료

--- client-app/src/component/board/BoardTable.jsx
+++ client-app/src/component/board/BoardTable.jsx
@@ -49,7 +49,10 @@ function BoardTable({ boards, navigate }) {
                   <TableCell>
                     <CardMedia
                       component="img"
-                      image={board.images[0]}
+                      image={
+                        board.images?.[0] ||
+                        "https://images.unsplash.com/photo-1584824486509-112e4181ff6b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                      }
                       alt={`board-${board.id}-img`}
                       sx={{
                         width: 100,
