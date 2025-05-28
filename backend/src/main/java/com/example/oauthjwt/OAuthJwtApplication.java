f122497018d2cf1ac429732d71f4c91ff05932aa
seung <seungchan753@naver.com>
2025-05-28 14:53:00 +0900

    스케줄 코드 삭제

--- backend/src/main/java/com/example/oauthjwt/OAuthJwtApplication.java
+++ backend/src/main/java/com/example/oauthjwt/OAuthJwtApplication.java
@@ -7,7 +7,6 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-// @EnableScheduling // 스케줄링 기능 활성화
 @Log4j2
 @EnableCaching
 public class OAuthJwtApplication {
