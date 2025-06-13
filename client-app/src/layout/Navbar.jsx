21e1bf29256a0df5e7c7d030117c76bbc988ab27
KatherineJH <ohhomoya@gmail.com>
2025-06-13 14:26:10 +0900

    업데이츠 누락된 #137 브랜치 재업

--- client-app/src/layout/Navbar.jsx
+++ client-app/src/layout/Navbar.jsx
@@ -33,19 +33,9 @@ const menuItems = [
     to: "/product/all",
   },
   {
-    text: "시간충전",
-    icon: <AccessTimeIcon color="secondary" fontSize="large" />,
-    to: "/payment/buy",
-  },
-  {
     text: "지역별",
     icon: <LocationOnIcon color="action" fontSize="large" />,
     to: "/product/list",
-  },
-  {
-    text: "커뮤니티",
-    icon: <ForumIcon color="disabled" fontSize="large" />,
-    to: "/board/list",
   },
   {
     text: "기부 참여",
@@ -59,6 +49,16 @@ const menuItems = [
       { text: "기부해요", to: "/product/donation" },
       { text: "봉사해요", to: "/product/volunteer" },
     ],
+  },
+  {
+    text: "커뮤니티",
+    icon: <ForumIcon color="disabled" fontSize="large" />,
+    to: "/board/list",
+  },
+  {
+    text: "시간충전",
+    icon: <AccessTimeIcon color="secondary" fontSize="large" />,
+    to: "/payment/buy",
   },
 ];
 
